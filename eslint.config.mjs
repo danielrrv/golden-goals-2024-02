a6479459e6fa1f8293fc1d79d380e2191f815252
Jose Saldaña <jose.saldana@autodesk.com>
2024-09-16 15:57:36 -0500

    feat: set up basics eslint, husky, vscode, prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.commonjs, ecmaVersion: 12 } },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "prettier/prettier": "error",
+      "no-console": "off",
+      eqeqeq: ["error", "always"],
+      curly: ["error", "all"],
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+      "comma-dangle": ["error", "never"],
+      "no-var": "error",
+      "prefer-const": "error",
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "no-multi-spaces": "error",
+      "space-before-function-paren": ["error", "never"],
+      "key-spacing": ["error", { beforeColon: false, afterColon: true }],
+      "space-infix-ops": "error",
+      "keyword-spacing": ["error", { before: true, after: true }],
+      "array-bracket-spacing": ["error", "never"],
+      "object-curly-spacing": ["error", "always"],
+      "block-spacing": ["error", "always"],
+      "arrow-spacing": ["error", { before: true, after: true }],
+      "no-trailing-spaces": "error",
+      "eol-last": ["error", "always"],
+    },
+  },
+];
